4fee28f65004f3bba42ae17337560024afc84582
gabokatta <gkatta@fi.uba.ar>
2023-09-29 18:40:03 -0300

    Fix: minor change of names.

--- app/tp1.ts
+++ app/tp1.ts
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     res.send("Hello from Express!")
 })
 
+app.get("/ping", async (req, res) => {
+    res.status(200).send("Application is Up!")
+})
+
 app.listen(port, () => {
     console.log(`now listening on: port:${port}`)
 })
