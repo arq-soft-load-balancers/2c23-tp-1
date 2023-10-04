e5f295fd4f3adb871f7c3c2e3c2bb85a24403c13
gabokatta <gkatta@fi.uba.ar>
2023-10-03 21:13:27 -0300

    Fix: Added Replication

--- app/services/quote.ts
+++ app/services/quote.ts
@@ -1,18 +1,16 @@
 import axios, { AxiosResponse } from "axios";
 import { Quote } from "./dtos/quote.js";
 import { ServiceError } from "./errors/service-error.js";
-import { redis } from "../tp1.js";
 import { MetricService, TimingType } from "./metrics.js";
 
 const QUOTE_ENDPOINT = "https://api.quotable.io/quotes/random?limit="
 const QUOTE_SINGLE_LIMIT = 1;
 const QUOTE_CACHE_LIMIT = 50;
-const QUOTE_TTL = 5;
-const CACHED_QUOTES_KEY = "quotes";
 
 export class QuoteService {
 
     public metricReporter = new MetricService("quotes");
+    private cachedQuotes: Quote[] = []
 
     async retrieveQuote(): Promise<Quote> {
         const response = await this.metricReporter.executeAndTime(async () => {
@@ -24,27 +22,15 @@ export class QuoteService {
     }
 
     async retrieveQuoteCached(): Promise<Quote> {
-        let cached_quotes = await redis.get(CACHED_QUOTES_KEY);
-        if (cached_quotes !== null) {
-            const parsed_quotes: Quote[] = JSON.parse(cached_quotes);
-            const single_quote: Quote = parsed_quotes.pop()!;    
-            if (parsed_quotes.length > 0) {
-                console.log(`FOUND CACHED QUOTES -> REMAINING: ${parsed_quotes.length}`)
-                await redis.set(CACHED_QUOTES_KEY, JSON.stringify(parsed_quotes), {EX: QUOTE_TTL})
-            }
-            else {
-                console.log(`RAN OUT OF CACHED QUOTES.`)
-                await redis.del(CACHED_QUOTES_KEY);
-            }
-            return new Quote(single_quote.author, single_quote.content)
-        }
-        else if (cached_quotes === null) {
-            const response = await axios.get<Quote[]>(`${QUOTE_ENDPOINT}${QUOTE_CACHE_LIMIT}`)
+        if (this.cachedQuotes.length === 0) {
+            const response = await this.metricReporter.executeAndTime(async () => {
+                return await axios.get<Quote[]>(`${QUOTE_ENDPOINT}${QUOTE_CACHE_LIMIT}`)
+            }, TimingType.EXTERNAL);
             this.sanitizeQuote(response)
-            await redis.set(CACHED_QUOTES_KEY, JSON.stringify(response.data), {EX: QUOTE_TTL})
-            console.log(`REFILLED CACHED QUOTES.`)
+            this.cachedQuotes = response.data;
         }
-        return this.retrieveQuote();
+        const single_quote: Quote = this.cachedQuotes.pop()!;
+        return new Quote(single_quote.author, single_quote.content)
     }
  
     private sanitizeQuote(quoteResponse: AxiosResponse<Quote[], any>) {
